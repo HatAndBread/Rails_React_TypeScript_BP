3773beca7995fd00bf0785a47b1143895a3dde1f
Joshua Hume <valencelectron@hotmail.com>
2021-07-12 19:19:07 +0900

    react wired up + eslint tsconfig

---
+++ d.ts
@@ -0,0 +1,20 @@
+declare module "*.png" {
+  const value: any;
+  export = value;
+}
+declare module "*.svg" {
+  const value: any;
+  export = value;
+}
+declare module "*.gif" {
+  const value: any;
+  export = value;
+}
+declare module "*.mp3" {
+  const value: any;
+  export = value;
+}
+declare module "*.wav" {
+  const value: any;
+  export = value;
+}
