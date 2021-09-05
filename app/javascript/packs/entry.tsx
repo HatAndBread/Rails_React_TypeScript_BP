6822a6690551a4f20b990eaf610617fbecc7c7ce
Joshua Hume <valencelectron@hotmail.com>
2021-09-05 11:34:43 +0900

    change dom content loaded to turbolinks:load

--- app/javascript/packs/entry.tsx
+++ app/javascript/packs/entry.tsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import { getInstanceVars } from "../lib/getInstanceVars";
 
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener("turbolinks:load", () => {
   ReactDOM.render(
     <App controllerData={getInstanceVars()} />,
 
